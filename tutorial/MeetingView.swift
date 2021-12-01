1c42bfedcbb0eaf28a1cd4b8d42a09c023512a9a
Ken Phetmany <ken.phetmany@student.uts.edu.au>
2021-12-01 17:43:37 +1100

    Created basic views

--- tutorial/MeetingView.swift
+++ tutorial/MeetingView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("Seconds Left").font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(Text("Time Remaining"))
+            .accessibilityValue(Text("10 Minutes"))
+            Circle().strokeBorder(lineWidth: 24, antialiased: true)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}){
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel(Text("Next speaker"))
+            }
+        }
+        .padding()
     }
 }
 
