f2ec7aed987879d0ad2653b06cd0c4cd9e9976c2
Ken Phetmany <ken.phetmany@student.uts.edu.au>
2021-12-01 17:11:55 +1100

    Test Commit

---
+++ tutorial/MeetingView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  tutorial
+//
+//  Created by Ken Phetmany on 27/11/21.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
