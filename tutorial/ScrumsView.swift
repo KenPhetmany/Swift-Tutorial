87415293f8f5b230d695b41dceffd95606b9b3dd
Ken Phetmany <ken.phetmany@student.uts.edu.au>
2022-01-08 19:23:40 +1100

    Added Detail Views and Basic Navigation

---
+++ tutorial/ScrumsView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+import Foundation
+
+struct ScrumView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)){
+                CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar{
+            Button(action: {}){
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
+        }
+    }
+}
+
+struct ScrumView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+        ScrumView(scrums: DailyScrum.sampleData)
+        }
+    }
+}
